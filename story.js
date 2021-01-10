9431d0d9e44085ce3270f83631bc799566a8b7b8
layla-lili <laylaisa.211@gmail.com>
2021-01-10 15:04:53 +0300

    finish my story

---
+++ story.js
@@ -0,0 +1,5 @@
+const myname="layla";
+const age=26;
+const hobby="playing with code";
+
+console.log(`Mr./Mrs. ${myname}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
