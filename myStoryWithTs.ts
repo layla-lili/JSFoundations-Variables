cc7072859bdeeec9009f69fb8230886954682f85
layla-lili <laylaisa.2112gmail.com>
2021-06-03 18:27:36 +0300

    with TS

---
+++ myStoryWithTs.ts
@@ -0,0 +1,10 @@
+export {};
+const mynamets = "layla";
+const agets = 27;
+const hobbyts = "playing with code";
+
+console.log(
+  `Mr./Mrs. ${mynamets}, in 33 years, you will 
+  be ${agets + 33} years old. By then you'll have decades o
+   ${hobbyts} practice!`
+);
---
+++ mystorywithts.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var mynamets = "layla";
+var agets = 27;
+var hobbyts = "playing with code";
+console.log(
+  "Mr./Mrs. " +
+    mynamets +
+    ", in 33 years, you will \n  be " +
+    (agets + 33) +
+    " years old. By then you'll have decades o\n   " +
+    hobbyts +
+    " practice!"
+);
